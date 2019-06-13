8c41fcb45762b2c42f110111ee7811155c5c118d
Facundo Cardozo <facu160996@gmail.com>
2019-06-12 22:10:44 -0300

    Programas finales

--- programas/programa2.py
+++ programas/programa2.py
@@ -3,51 +3,56 @@
 import sys
 import io
 import nltk
-
+from nltk.tree import Tree
 
 def tokenize(text):
     return list(text)
 
-
 def parse(s):
     grammar = """
-    S -> '(' S ')' | S '+' S | S '*' S | N 
-
-    N -> N '0' | N '1' | N '2' | N '3' | N '4' | N '5' | N '6' | N '7' | N '8' | N '9' | '1' | '3' | '2' | '4' | '5' | '6' | '7' | '8' | '9'
-
-    """
+    S ->'(' S ')' | S '*' S | S '+' S | NUM
+    NUM -> '0' |'1' | '2' | '3' |'4' | '5' | '6' | '7' | '8' | '9' | '0' NUM | '1' NUM | '2' NUM | '3' NUM | '4' NUM | '5' NUM | '6' NUM | '7' NUM | '8' NUM | '9' NUM 
+    """        
     grammar = nltk.CFG.fromstring(grammar)
     s_tok = tokenize(s.strip())
     parser = nltk.LeftCornerChartParser(grammar)
-    tree = [t for t in parser.parse(s_tok)][:1]
+    tree=[t for t in parser.parse(s_tok)][:1]
     return tree
+
+def recorrer(tree):
+    resultado=""
+    for subtree in tree:
+        if type(subtree) == Tree:
+            if(subtree.label()=="S"):
+                resultado=recorrer(subtree)+resultado
+            else:
+                resultado=resultado+recorrer(subtree)
+                
+        else:
+            if(subtree=="("):
+                subtree=")"
+            else:
+                if(subtree==")"):
+                    subtree="("  
+            resultado=subtree+resultado
+    return resultado
 
 
 if __name__ == '__main__':
     archivo_entrada = sys.argv[1]
     archivo_salida = sys.argv[2]
     f = io.open(archivo_entrada, 'r', newline='\n', encoding='utf-8')
-    s = f.read()  
+    s = f.read()
     f.close()
-    
-    #s = '(12*(9+(115*12)))+(10+5)'
-    # ejemplos
-    # entrada7 = '7*(45+16)' -> (16+45)*7
-    # entrada8 = '7+(45*)' -> NO PERTENECE
-    # entrada9 = '(12*(9+(115*12)))+(10+5)' -> (5+10)+(((12*115)+9)*12)
-      
-  
     try:
       tree = parse(s)
+      resultado=recorrer(tree)
       if tree:
-          salida = "PERTENECE"
+          salida = resultado
       else:
           salida = "NO PERTENECE"
     except ValueError:
       salida = "NO CUBRE"
-    #print(salida)
     f = io.open(archivo_salida, 'w', newline='\n', encoding='utf-8')
     f.write(salida)
     f.close()
-
-
--- programas/programa3.py
+++ programas/programa3.py
@@ -4,6 +4,7 @@ import sys
 import io
 import nltk
 import re
+from nltk.tree import Tree
 
 def tokenize(text):
 	return list(text)
@@ -36,31 +37,52 @@ def parse(s):
 
 	grammar = nltk.CFG.fromstring(grammar)
 	s_tok = tokenize(s.strip())
-	#BottomUpLeftCornerChartParser
 	parser = nltk.LeftCornerChartParser(grammar)
-	# parser = nltk.LeftCornerChartParser(grammar)
 	tree = [t for t in parser.parse(s_tok)][:1]
 	return tree
+	
+def espacio(y):
+	espacio = ''
+	for i in range(0,y):
+		espacio += '    '
+	return espacio
 
-
+def jsontoyaml(tree, n):
+	resultado=""
+	for subtree in tree:
+		if type(subtree) == Tree:
+			if(subtree.label()=="S"):
+				resultado=resultado+jsontoyaml(subtree, n+1)
+			else:
+				resultado=resultado+jsontoyaml(subtree, n)
+                
+		else:
+			if(subtree=='{' or subtree==','):
+				ident = espacio(n)
+				if n != 0:
+					resultado += '\n' + ident
+				else:
+					if subtree != '{':
+						resultado += '\n'
+			else:
+				if(subtree!='}' and subtree!='"'):
+					resultado=resultado+subtree
+				if(subtree==":"):
+					resultado += ' '
+	return resultado
+	
+	
 if __name__ == '__main__':
 	archivo_entrada = sys.argv[1]
 	archivo_salida = sys.argv[2]
 	f = io.open(archivo_entrada, 'r', newline='\n', encoding='utf-8')
 	s = f.read()  
-	f.close()
-	
-	
-	# ejemplos
-	#entrada7 = '{"nombre":"s","valor_r":255,"valor_g":0,"valor_b":0}' #-> PERTENECE
-	#entrada8 = '{"colores":{"rojo":{"r":255,"g":0,"b":0},"azul":{"r":0,"g":0,"b":255}}}' #-> PERTENECE
-	#entrada9 = '{"asd":"asd","a:255}' #-> NO PERTENECE
-	#s = entrada7  
+	f.close() 
   
 	try:
 	  tree = parse(s)
 	  if tree:
-		  salida = "PERTENECE"
+		  salida = jsontoyaml(tree, -1)
 	  else:
 		  salida = "NO PERTENECE"
 	except ValueError:
